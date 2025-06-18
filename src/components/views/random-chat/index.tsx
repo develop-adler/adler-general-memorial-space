343b95c1c58e51f09b72c5f639a4e59ece90d15c
berryg23 <159242361+berryg23@users.noreply.github.com>
2025-06-18 23:48:52 +0900

    fix ios notch area overlap

--- src/components/views/random-chat/index.tsx
+++ src/components/views/random-chat/index.tsx
@@ -1,8 +1,10 @@
 "use client";
-
+import { useEffect } from "react";
 import dynamic from "next/dynamic";
 
 import { useResponsive } from "@/hooks/useResponsive";
+import { useDetectBrowser } from "@/hooks/useDetectBrowser";
+
 
 const DesktopRandomPage = dynamic(() => import("./index.desktop"), {
   ssr: false,
@@ -13,6 +15,15 @@ const MobileRandomPage = dynamic(() => import("./index.mobileTab"), {
 export const RandomChat: React.FC = () => {
   const isMobile = useResponsive("Phone");
   const isTablet = useResponsive("Tablet");
+  const { isIOSWebView } = useDetectBrowser();
+
+  useEffect(() => {
+    if (isIOSWebView) {
+      document.documentElement.classList.add('wkwebview-ios');
+    } else {
+      document.documentElement.classList.remove('wkwebview-ios');
+    }
+  }, [isIOSWebView]);
 
   return <>{isMobile || isTablet ? <MobileRandomPage /> : <DesktopRandomPage />}</>;
 };
